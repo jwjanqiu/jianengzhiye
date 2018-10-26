4fb06dee8b05509dad4f7bd1ffd2796df69dcee6
QiuYiEr <jwjanqiu@outlook.com>
2018-10-26 12:42:52 +0800

    更新database配置

--- application/database.php
+++ application/database.php
@@ -13,13 +13,13 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '127.0.0.1',
+    'hostname'        => '120.79.10.159',
     // 数据库名
     'database'        => 'jia_neng_zhi_ye',
     // 用户名
-    'username'        => 'root',
+    'username'        => 'jia_neng_zhi_ye',
     // 密码
-    'password'        => '7ba1b3303d1f1a79',
+    'password'        => 'mB7DCraydd',
     // 端口
     'hostport'        => '3306',
     // 连接dsn
